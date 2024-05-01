79f06772911c6a856be7c8e38868582dbdbd9531
Mohamed Walid <medo4bmw@gmail.com>
2024-05-01 08:22:35 +0300

    reverting some changes to clientRoutes and index.js

--- backend/Routes/clientRoutes.js
+++ backend/Routes/clientRoutes.js
@@ -5,10 +5,10 @@ const Auth = require("../controllers/authController")
 const router = express.Router()
 
 router.post("/createOrder", Auth.protect, Auth.restrictTo("client"), clientOrders.createOrder)
-router.get("/orders", Auth.protect, Auth.restrictTo("client"), clientOrders.getAllOrders);
+router.get("/getAllOrders", Auth.protect, Auth.restrictTo("client"), clientOrders.getAllOrders);
 
 router
-.route("/orders/:id")
+.route("/:id")
 .get(Auth.protect, Auth.restrictTo("client"), clientOrders.getOrder)
 .patch(Auth.protect, Auth.restrictTo("client"), clientOrders.updateOrder)
 .delete(Auth.protect, Auth.restrictTo("client"), clientOrders.deleteOrder);
