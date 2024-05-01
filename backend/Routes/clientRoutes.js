51cc3fa18ba97359d5842e421de04d952936cfb0
L0da <71377573+L0daW@users.noreply.github.com>
2024-05-01 10:20:23 +0300

    changes to clientController.js clientRoues.js and index.js (#6)
    * adding ".env" to the .gitignore file
    
    * editing ".gitignore ", adding "*.env and env.txt"
    
    * adding more clientRoutes and adjusting index.js file for it
    
    * reverting some changes to clientRoutes and index.js
    
    * WIP: c14c038 editing ".gitignore ", adding "*.env and env.txt"

--- backend/Routes/clientRoutes.js
+++ backend/Routes/clientRoutes.js
@@ -6,5 +6,12 @@ const router = express.Router()
 
 router.get("/nearestDelivery", Auth.protect, Auth.restrictTo("client"), clientOrders.nearestDelivery)
 router.post("/createOrder", Auth.protect, Auth.restrictTo("client"), clientOrders.createOrder)
+router.get("/getAllOrders", Auth.protect, Auth.restrictTo("client"), clientOrders.getAllOrders);
+
+router
+.route("/:id")
+.get(Auth.protect, Auth.restrictTo("client"), clientOrders.getOrder)
+.patch(Auth.protect, Auth.restrictTo("client"), clientOrders.updateOrder)
+.delete(Auth.protect, Auth.restrictTo("client"), clientOrders.deleteOrder);
 
 module.exports = router
