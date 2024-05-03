5ee8a95b06c6d19a6b726eb6f771a8de4b6df333
Mohamed Walid <medo4bmw@gmail.com>
2024-05-03 18:57:25 +0300

    removing required fields

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,11 +8,9 @@ const pointSchema = new mongoose.Schema({
   type: {
     type: String,
     enum: ['Point'],
-    required: true
   },
   coordinates: {
     type: [Number],
-    required: true
   }
 });
 
